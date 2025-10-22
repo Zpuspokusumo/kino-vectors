5af86c8d87ec835be8d616eb4870c9e0059ab56e
Zpuspokusumo <zpuspokusumo@gmail.com>
2025-10-22 19:36:59 +0700

    placeholder main

---
+++ embeddings-service/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	repository "kino-vectors/services/embedding-service"
+)
+
+func main() {
+	repository.Onnxservice("I am a witch and i like casting spells")
+
+	//time.Sleep(30 * time.Second)
+}
