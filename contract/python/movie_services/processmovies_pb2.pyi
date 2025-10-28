45b255ecaf7aaabdfaef90e95341a26f479d892b
Zpuspokusumo <zpuspokusumo@gmail.com>
2025-10-28 20:05:23 +0700

    grpc contract for dag to golang-embedding to qdrant etl process

---
+++ contract/python/movie_services/processmovies_pb2.pyi
@@ -0,0 +1,41 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from collections.abc import Iterable as _Iterable, Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class MovieInfo(_message.Message):
+    __slots__ = ("title", "director", "year", "genre", "actors", "summary")
+    TITLE_FIELD_NUMBER: _ClassVar[int]
+    DIRECTOR_FIELD_NUMBER: _ClassVar[int]
+    YEAR_FIELD_NUMBER: _ClassVar[int]
+    GENRE_FIELD_NUMBER: _ClassVar[int]
+    ACTORS_FIELD_NUMBER: _ClassVar[int]
+    SUMMARY_FIELD_NUMBER: _ClassVar[int]
+    title: str
+    director: str
+    year: str
+    genre: str
+    actors: _containers.RepeatedScalarFieldContainer[str]
+    summary: str
+    def __init__(self, title: _Optional[str] = ..., director: _Optional[str] = ..., year: _Optional[str] = ..., genre: _Optional[str] = ..., actors: _Optional[_Iterable[str]] = ..., summary: _Optional[str] = ...) -> None: ...
+
+class MovieInfos(_message.Message):
+    __slots__ = ("Movies",)
+    MOVIES_FIELD_NUMBER: _ClassVar[int]
+    Movies: _containers.RepeatedCompositeFieldContainer[MovieInfo]
+    def __init__(self, Movies: _Optional[_Iterable[_Union[MovieInfo, _Mapping]]] = ...) -> None: ...
+
+class ProcessMovieResponse(_message.Message):
+    __slots__ = ("status", "message", "items_processed", "unprocessed_items")
+    STATUS_FIELD_NUMBER: _ClassVar[int]
+    MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    ITEMS_PROCESSED_FIELD_NUMBER: _ClassVar[int]
+    UNPROCESSED_ITEMS_FIELD_NUMBER: _ClassVar[int]
+    status: int
+    message: str
+    items_processed: int
+    unprocessed_items: _containers.RepeatedScalarFieldContainer[str]
+    def __init__(self, status: _Optional[int] = ..., message: _Optional[str] = ..., items_processed: _Optional[int] = ..., unprocessed_items: _Optional[_Iterable[str]] = ...) -> None: ...
